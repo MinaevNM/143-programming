ddca5a9a14cc65086f3ae22e0ea2dde7487a46d6
Nikita <nikita_340@mail.ru>
2014-03-29 13:03:31 +0300

    Create gvehicle.h

---
+++ gvehicle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "vehicle.h"
+
+class GroundVehicle: public Vehicle
+{
+private:
+	double wheel_diam;
+public:
+	GroundVehicle( int _motor_id, double _wheel_diam ) : 
+	  Vehicle(_motor_id), wheel_diam(_wheel_diam)
+	{
+	}
+  void print_full()
+	{
+		print_motor();
+		cout << "Wheel_diam = " << wheel_diam << endl;
+	}
+};
